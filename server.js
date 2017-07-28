e591e52b3aece35bb7ac4fd16d79a3dafb7bf923
weswhitney <wesleywhitney@gmail.com>
2017-07-27 21:45:35 -0600

    http module

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-import https from 'https';
+import http from 'http';
 
-https.get('https://www.lynda.com', res => {
-	console.log("response status code: ", res.statusCode);
+// using https module as a client:
 
-	res.on('data', chunk => {
-		console.log(chunk.toString());
-	});
+// https.get('https://www.lynda.com', res => {
+// 	console.log("response status code: ", res.statusCode);
+
+// 	res.on('data', chunk => {
+// 		console.log(chunk.toString());
+// 	});
+// });
+
+const server = http.createServer((req, res) => {
+	res.write('Hello\n');
+	setTimeout(() => {
+		res.write('I can stream!\n');
+		res.end();
+	}, 3000);
 });
+
+server.listen(8080);
+
+server.on('request');
+
+
