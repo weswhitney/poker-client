0abc7b547202bca33a7a59be8d3357f6841ad4ef
weswhitney <wesleywhitney@gmail.com>
2017-07-29 19:10:28 -0600

    ejs and views

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ import apiRouter from './api';
 import express from 'express';
 const server = express();
 
+server.set('view engine', 'ejs'); // by default express will look for a views folder
+
 server.get('/', (req, res) => {
-	res.send('hello express wat');
+	res.render('index', {
+		content: 'Hello express and <em>ejs!</em>'
+	});
 });
 
 server.use('/api', apiRouter);
