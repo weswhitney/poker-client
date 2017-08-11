c30165e947849aaf09e4b2b8720131233b48bb49
tedrand <trand@sovrn.com>
2017-08-10 22:34:13 -0600

    deleted cruft in config.js

--- config.js
+++ config.js
@@ -2,11 +2,6 @@ const env = process.env;
 
 export const nodeEnv = env.NODE_ENV || 'development';
 
-export const logStars = function(message) {
-	console.info('***************');
-	console.info(message);
-}
-
 export default {
 	port: env.PORT || 8080
 };
