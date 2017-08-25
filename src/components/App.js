848aa9a5d290f250ae51f9ea505b69cda4d1b77c
Wesley  Whitney <wesleywhitney@gmail.com>
2017-08-25 08:19:35 -0600

    organized components and deleted unused components

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
-// import Footer from './Footer';
-// import Header from './Header';
-// import Main from './Main';
-// import React from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Footer from './Footer';
+import Header from './Header';
+import Main from './Main';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// const App extends React.component {
-//   render() {
-//     return (
-//       <div>
-//         <Header />
-//         <Main />
-//         <Footer />
-//       </div>
-//     )
-//   }
-// }
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Main />
+          <Footer />
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
 
-// export default App;
+export default App;
